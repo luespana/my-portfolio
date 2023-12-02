b2954cf2cbe4ff024e0a2eee53c7a1b3be99a9cf
Lucía España <99997724+luespana@users.noreply.github.com>
2023-12-02 18:34:35 -0300

    finished nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     setMenuOpen(!menuOpen);
   };
   return (
-    <nav className="w-full fixed top-0 left-0 right-0 h-24">
+    <nav className="w-full fixed top-0 left-0 right-0 h-24 sm:shadow-sm">
       <div className="flex justify-between items-center h-24 w-full px-4 2xl:px-16">
         <Link href="/">
           <img src="images/logo.png" />
@@ -31,10 +31,10 @@ export default function NavBar() {
               <li className="text-base ml-10">Contact</li>
             </Link>
             <Link href="/">
-              <LinkedinLogo size={30} className="ml-4" />
+              <LinkedinLogo size={25} className="ml-10" />
             </Link>
             <Link href="/">
-              <GithubLogo size={30} className="ml-8" />
+              <GithubLogo size={25} className="ml-8" />
             </Link>
           </ul>
         </div>
@@ -45,13 +45,68 @@ export default function NavBar() {
       <div
         className={
           menuOpen
-            ? "fixed right-0 top-0 w-[65%] sm:hidden h-screen bg-slate-300 p-10 ease-in duration-500"
-            : "fixed top-0 left-[100%] p-10 ease-in duration-500"
+            ? "fixed right-0 top-0 w-[65%] sm:hidden h-screen bg-white p-10 shadow-lg transition-transform ease-in-out duration-300 transform translate-x-0"
+            : "fixed top-0 left-[100%] p-10 transition-transform ease-in-out duration-300 transform translate-x-full"
         }
       >
         <div className="flex w-full items-center justify-end">
-          <div onClick={handleNav} className="cursor-pointer"></div>
-          <X size={30} />
+          <div onClick={handleNav} className="cursor-pointer">
+            <X size={30} />
+          </div>
+        </div>
+        <div className="flex-col py-4">
+          <ul>
+            <Link href="/">
+              <li
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                className="py-4 cursor-pointer"
+              >
+                Home
+              </li>
+            </Link>
+            <Link href="/">
+              <li
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                className="py-4 cursor-pointer"
+              >
+                About
+              </li>
+            </Link>
+            <Link href="/">
+              <li
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                className="py-4 cursor-pointer"
+              >
+                Tech Skills
+              </li>
+            </Link>
+            <Link href="/">
+              <li
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                className="py-4 cursor-pointer"
+              >
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                className="py-4 cursor-pointer"
+              >
+                Contact
+              </li>
+            </Link>
+          </ul>
         </div>
       </div>
     </nav>
